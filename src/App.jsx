a1efe7032fb3ec3b285d184ba8363157f2f88691
webdev-it <webdevit3@gmail.com>
2024-03-10 10:20:07 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div>
+      <h1 className='text-3xl underline font-bold'>Tic Toc Toe</h1>
+    </div>
+  );
+};
+
+export default App;
