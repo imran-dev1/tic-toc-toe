cc6aa1ad0b773d377e219c680f07294bb64cef12
webdev-it <webdevit3@gmail.com>
2024-03-11 09:06:10 +0600

    Winning confetti added

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,64 @@
 import { useState } from "react";
 import Board from "./Board";
 import History from "./History";
+import ReactConfetti from "react-confetti";
 
 const Game = () => {
    const [squares, setSquares] = useState(Array(9).fill(null));
    const [isXNext, setIsXNext] = useState(true);
+
+   //Winner Calculation
+   const calculateWinner = (squares) => {
+      const winningLines = [
+         [0, 1, 2],
+         [3, 4, 5],
+         [6, 7, 8],
+         [0, 3, 6],
+         [1, 4, 7],
+         [2, 5, 8],
+         [0, 4, 8],
+         [2, 4, 6],
+      ];
+
+      for (let index = 0; index < winningLines.length; index++) {
+         const [a, b, c] = winningLines[index];
+         if (
+            squares[a] &&
+            squares[b] === squares[a] &&
+            squares[c] === squares[a]
+         ) {
+            return squares[a];
+         }
+      }
+      return null;
+   };
+   const winner = calculateWinner(squares);
+   let status;
+   if (winner) {
+      status = `Winner: ${winner}`;
+   } else {
+      status = `Next Player: ${isXNext ? "X" : "O"}`;
+   }
    return (
       <div className="h-screen flex justify-center items-center">
-         <div className=" bg-black/35 backdrop-blur-md p-20  flex  gap-5 border border-purple-900 border-4 rounded-lg text-white">
+         <div className=" bg-black/35 backdrop-blur-md p-20 flex gap-5 border border-purple-900 border-4 rounded-lg text-white">
             <div className="flex flex-col gap-4">
                <Board
                   squares={squares}
                   setSquares={setSquares}
                   isXNext={isXNext}
                   setIsXNext={setIsXNext}
+                  winner={winner}
+                  status={status}
                />
+                {winner && <button className="">Play Again</button>}
             </div>
+           
             <div>
                <History />
             </div>
          </div>
+         {winner && <ReactConfetti />}
       </div>
    );
 };
--- src/Board.jsx
+++ src/Board.jsx
@@ -1,39 +1,6 @@
 import Square from "./Square";
 
-const Board = ({ squares, setSquares, isXNext, setIsXNext }) => {
-
-   //Winner Calculation
-   const calculateWinner = (squares) => {
-      const winningLines = [
-         [0, 1, 2],
-         [3, 4, 5],
-         [6, 7, 8],
-         [0, 3, 6],
-         [1, 4, 7],
-         [2, 5, 8],
-         [0, 4, 8],
-         [2, 4, 6],
-      ];
-
-      for (let index = 0; index < winningLines.length; index++) {
-         const [a, b, c] = winningLines[index];
-         if (
-            squares[a] &&
-            squares[b] === squares[a] &&
-            squares[c] === squares[a]
-         ) {
-            return squares[a];
-         }
-      }
-      return null;
-   };
-   const winner = calculateWinner(squares);
-   let status;
-   if (winner) {
-      status = `Winner: ${winner}`;
-   } else {
-      status = `Next Player: ${isXNext ? "X" : "O"}`;
-   }
+const Board = ({ squares, setSquares, isXNext, setIsXNext,winner,status }) => {
 
    //Handle Square Click
    const handleClick = (index) => {
@@ -68,6 +35,7 @@ const Board = ({ squares, setSquares, isXNext, setIsXNext }) => {
             <Square handleClick={handleClick} index="7" squares={squares} />
             <Square handleClick={handleClick} index="8" squares={squares} />
          </div>
+         
       </>
    );
 };
